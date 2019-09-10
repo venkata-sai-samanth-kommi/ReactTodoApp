e7abb3ef6bceb5027380cef1f681188aa69b6119
sai12999 <sai12999.k@gmail.com>
2019-09-10 15:49:29 +0530

    Updates

---
+++ precache-manifest.81ff2b8af0f60029fca6879e10941d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2485d70208eeca97aec2d4734b7493",
+    "url": "/ReactTodoApp/index.html"
+  },
+  {
+    "revision": "682a9494894d488c7f8b",
+    "url": "/ReactTodoApp/static/css/main.a2e95792.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ReactTodoApp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "682a9494894d488c7f8b",
+    "url": "/ReactTodoApp/static/js/main.926e25f2.chunk.js"
+  },
+  {
+    "revision": "fcfe4318834a1472d029",
+    "url": "/ReactTodoApp/static/js/runtime~main.a539cb81.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactTodoApp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
